b3c734ecbda1e19c882cab0bb771b29e819907c8
tanitta <ttata.trit@gmail.com>
2016-03-31 12:44:59 +0900

    Update examples.

--- examples/timer/source/app.d
+++ examples/timer/source/app.d
@@ -6,26 +6,36 @@ import specassert.core;
 class Timer(N) {
 	public{
 		void start(){};
+		void count(){};
 		void stop(){};
+		N time(){return _time;}
 	}//public
 
 	private{
+		N _time;
 	}//private
 }//class Timer
 
 @spec unittest{
+	alias N = double;
 	specAssert(__traits(compiles, (){
-		auto timer = new Timer!double;
+		auto timer = new Timer!N;
 	}));
 	
-	auto timer = new Timer!double;
+	auto timer = new Timer!N;
 	specAssert(__traits(hasMember, timer, "start"));
+	specAssert(__traits(hasMember, timer, "count"));
 	specAssert(__traits(hasMember, timer, "stop"));
-	specAssert(!__traits(hasMember, timer, "stop"));
+	
+	specAssert(timer.time == N(0));
+	timer.start;
+	timer.count;
+	timer.stop;
+	specAssert(timer.time == N(1));
 }
 
+// if you write main block, describe "mixin SpecAssert" within conditional compile.
 version(unittest){
-	mixin SpecAssert;
 }else{
 	void main(){}
 }
