9bd4a19ba0b270831ae0fa8d41096ad2b669e152
tanitta <ttata.trit@gmail.com>
2016-03-31 18:33:38 +0900

    Enable to view assert condition.

--- source/specassert/core.d
+++ source/specassert/core.d
@@ -4,6 +4,7 @@ import std.stdio;
 import core.exception;
 
 enum spec;
+
 /++
 +/
 struct describe{
@@ -12,9 +13,57 @@ struct describe{
 
 /++
 +/
-private class TestCorrecter {
+class Spec{
 	public{
-		alias T = Test;
+		this(in string file, in size_t line, in string mod, in string prettyFunction, in string msg, in bool isSuccess){
+			_file = file;
+			_line = line;
+			_mod= mod;
+			_prettyFunction = prettyFunction;
+			_msg = msg;
+			_isSuccess = isSuccess;
+		}
+		@property{
+			string file()const{return _file;}
+			size_t line()const{return _line;}
+			string mod()const{return _mod;}
+			string prettyFunction()const{return _prettyFunction;}
+			string msg()const{return _msg;}
+			bool isSuccess()const{return _isSuccess;}
+		}
+	}//public
+
+	private{
+		string _file;
+		size_t _line;
+		string _mod;
+		string _prettyFunction;
+		string _msg;
+		bool _isSuccess;
+	}//private
+}//class SpecCase
+
+string[] parsedAssertCondition(Spec s){
+	import std.file;
+	import std.conv;
+	string source = read(s.file).to!string;
+	
+	import std.string;
+	string[] lines = source.split("\n");
+	
+	import std.array;
+	import specassert.parser;
+	return lines[s.line-1..$].join("\n").parseSource;
+}
+unittest{
+	// parsedAssertCondition(new Spec("source/specassert/core.d", 59, "hoge", "func", "msg", true)).writeln;
+}
+
+/++
++/
+private class SpecCorrecter {
+	public{
+		alias T = Spec;
 		void add(T test){
 			_tests ~= test;
 		}
@@ -48,9 +97,10 @@ private class TestCorrecter {
 				with(test){
 					auto statusColor = isSuccess?"green":"red";
 					text(file, "(", line, ")").color(statusColor).writeln;
+					test.parsedAssertCondition[0].text.color(statusColor).writeln;
 					if(msg!="")text(msg).color(statusColor).writeln;
-					text("mod     : ", mod).color(statusColor).writeln;
-					text("prettyFunction     : ", prettyFunction).color(statusColor).writeln;
+					// text("mod     : ", mod).color(statusColor).writeln;
+					// text("prettyFunction     : ", prettyFunction).color(statusColor).writeln;
 					"".writeln;
 				}
 			}
@@ -71,42 +121,11 @@ private class TestCorrecter {
 
 
 
-/++
-+/
-class Test{
-	public{
-		this(in string file, in size_t line, in string mod, in string prettyFunction, in string msg, in bool isSuccess){
-			_file = file;
-			_line = line;
-			_mod= mod;
-			_prettyFunction = prettyFunction;
-			_msg = msg;
-			_isSuccess = isSuccess;
-		}
-		@property{
-			string file()const{return _file;}
-			size_t line()const{return _line;}
-			string mod()const{return _mod;}
-			string prettyFunction()const{return _prettyFunction;}
-			string msg()const{return _msg;}
-			bool isSuccess()const{return _isSuccess;}
-		}
-	}//public
-
-	private{
-		string _file;
-		size_t _line;
-		string _mod;
-		string _prettyFunction;
-		string _msg;
-		bool _isSuccess;
-	}//private
-}//class TestCase
 
 
 
 
-private void processTest(in string file, in int line, in string moduleName, in string prettyFunction, in bool isSuccess, in string message){
+private void processSpec(in string file, in int line, in string moduleName, in string prettyFunction, in bool isSuccess, in string message){
 	
 	import std.conv;
 	if(testCorrector.isFailFast && !isSuccess){
@@ -114,14 +133,14 @@ private void processTest(in string file, in int line, in string moduleName, in s
 		error.msg = message;
 		throw error;
 	}else{
-		testCorrector.add(new Test(file, line, moduleName, prettyFunction, message, isSuccess));
+		testCorrector.add(new Spec(file, line, moduleName, prettyFunction, message, isSuccess));
 	}
 }
 
 import std.exception;
 bool specAssert(string file = __FILE__, int line = __LINE__,  string mod= __MODULE__, string prettyFunction = __PRETTY_FUNCTION__)(bool isSuccess){
 	try{
-		processTest(file, line, mod, prettyFunction, isSuccess, "");
+		processSpec(file, line, mod, prettyFunction, isSuccess, "");
 	}catch(AssertError err){
 		with(err){
 			auto error = new AssertError(file, line);
@@ -134,7 +153,7 @@ bool specAssert(string file = __FILE__, int line = __LINE__,  string mod= __MODU
 
 bool specAssert(string file = __FILE__, int line = __LINE__,  string mod= __MODULE__, string prettyFunction = __PRETTY_FUNCTION__)(bool isSuccess, string message){
 	try{
-		processTest(file, line, mod, prettyFunction, isSuccess, message);
+		processSpec(file, line, mod, prettyFunction, isSuccess, message);
 	}catch(AssertError err){
 		with(err){
 			auto error = new AssertError(file, line);
@@ -154,7 +173,5 @@ mixin template SpecAssert(){
 	}
 }
 
-static __gshared testCorrector = new TestCorrecter;
-version(spec){
-	mixin SpecAssert;
-}
+static __gshared testCorrector = new SpecCorrecter;
+mixin SpecAssert;
